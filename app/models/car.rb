c09cdac10d529357ed42c0e7ed5fdf1496e87640
jonny kang <jono.kang@gmail.com>
2014-05-14 09:42:30 -0700

    adding more validations and alerts

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,5 +3,7 @@ class Car
   field :name, type: String
   field :model, type: String
   field :year, type: Integer
+  field :, type: Integer
   validates :year, numericality: { greater_than: 2000 }
+  validates_inclusion_of :transmission, in: [ "auto", "manual" ], message: "pick body type"
 end
